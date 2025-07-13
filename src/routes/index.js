980439ba74db8bdd4fcf3e3ec304017e943d15ca
Dima <shimdim@gmail.com>
2025-07-13 21:59:02 +0200

    Add tools endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const accountRoutes = require('./account');
 const botsRoutes = require('./bots');
 const dialogsRoutes = require('./dialogs');
+const toolsRoutes = require('./tools');
 
 // Mount routes
+router.use('/', toolsRoutes); // This should come first to handle the root path
 router.use('/account', accountRoutes);
 router.use('/bots', botsRoutes);
 router.use('/dialogs', dialogsRoutes);
